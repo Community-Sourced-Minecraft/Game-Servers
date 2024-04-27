724e08a6ae8ee553aa42d2f24e6c519150903fc9
DevMiner <devminer@devminer.xyz>
2024-04-27 20:12:32 +0200

    fix(lobby): prevent people from falling into the void

--- lobby/src/main/java/com/github/CommunitySourcedMinecraft/Main.java
+++ lobby/src/main/java/com/github/CommunitySourcedMinecraft/Main.java
@@ -4,6 +4,7 @@ import net.hollowcube.polar.PolarLoader;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerMoveEvent;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.network.packet.client.play.ClientChatAckPacket;
@@ -15,6 +16,8 @@ import java.nio.file.Path;
 
 public class Main {
 	static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	private static final Pos SPAWN = new Pos(0, 10, 0, 180f, 0f);
 
 	public static void main(String[] args) throws IOException {
 		var proxySecret = System.getenv("PROXY_SECRET");
@@ -40,7 +43,16 @@ public class Main {
 			event.setSpawningInstance(instanceContainer);
 			event
 				.getPlayer()
-				.setRespawnPoint(new Pos(0, 42, 0, 180f, 0f));
+				.setRespawnPoint(SPAWN);
+		});
+		globalEventHandler.addListener(PlayerMoveEvent.class, event -> {
+			if (event
+				.getNewPosition()
+				.blockY() > 0) return;
+
+			event
+				.getPlayer()
+				.teleport(SPAWN);
 		});
 
 		minecraftServer.start("0.0.0.0", 25565);
